4d9c88e94c5dd6bc08d3ae9d336a190cd7dde619
i <iremturanli@hotmail.com>
2022-10-18 11:31:48 +0300

    last

---
+++ Controllers/UserController.cs
@@ -0,0 +1,69 @@
+﻿using JwtApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace JwtApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpGet("Avukat")]
+        [Authorize(Roles = "Avukat")]
+        public IActionResult AvukatEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+
+            return Ok($"Hi {currentUser.Username}, you are an {currentUser.Role}");
+        }
+
+
+        [HttpGet("TBB")]
+        [Authorize(Roles = "TBBKullanicisi")]
+        public IActionResult TBBEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+
+            return Ok($"Hi {currentUser.Username}, you are a {currentUser.Role}");
+        }
+
+        [HttpGet("Stajyer")]
+        [Authorize(Roles = "Stajyer")]
+        public IActionResult StajyerEndpoint()
+        {
+            var currentUser = GetCurrentUser();
+
+            return Ok($"Hi {currentUser.Username}, you are an {currentUser.Role}");
+        }
+
+        [HttpGet("Public")]
+        public IActionResult Public()
+        {
+            return Ok("Hi, you're on public property");
+        }
+
+        private UserModel GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+
+                return new UserModel
+                {
+                    Username = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value,
+                    Role = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Role)?.Value
+                };
+            }
+            return null;
+        }
+    }
+}
---
+++ Models/UserConstants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtApp.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+
+
+            new UserModel() { Username = "irem", Password = "12345", Role = "Avukat" },
+            new UserModel() { Username = "berkay", Password = "MyPass_w0rd", Role = "TBBKullanicisi" },
+            new UserModel() { Username = "hussain", Password = "MyPass_w0rd", Role = "Stajyer" }
+         };
+       }
+}
+
+// {"Username":"berkay", "Password":"MyPass_w0rd"}
